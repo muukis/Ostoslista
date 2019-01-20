112f779ac2f8ec6e2c484d506311b7af343a5afc
Mikko Andersson <mikko.andersson@vincit.fi>
2019-01-20 20:01:19 +0200

    About page and then some

--- API/OstoslistaAPI/Pages/CreateNewShopper.cshtml.cs
+++ API/OstoslistaAPI/Pages/CreateNewShopper.cshtml.cs
@@ -1,30 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace OstoslistaAPI.Pages
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class CreateNewShopperModel : PageModel
     {
-        private static Random random = new Random();
+        private static readonly Random _random = new Random();
 
+        /// <summary>
+        /// 
+        /// </summary>
         public void OnGet()
         {
 
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public string GetRandomShopperName()
         {
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-            return new string(Enumerable.Repeat(chars, random.Next(4, 6))
+            return new string(Enumerable.Repeat(chars, _random.Next(4, 6))
                 .Select(s =>
                 {
-                    char c = s[random.Next(s.Length)];
-                    return random.Next(2) == 0 ? c : c.ToString().ToLower()[0];
+                    char c = s[_random.Next(s.Length)];
+                    return _random.Next(2) == 0 ? c : c.ToString().ToLower()[0];
                 }).ToArray());
         }
     }
